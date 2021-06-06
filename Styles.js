f21590e1617edbf162d3ef3d5b6f92f15c5da80b
Kevin R Salger <kevin.salger@student.csulb.edu>
2021-06-05 21:24:59 -0700

    21st

--- Styles.js
+++ Styles.js
@@ -48,7 +48,7 @@ export default StyleSheet.create({
   rowbox1: {
       //width: 400,
       height: 200,
-      flexDirection: "row",
+      flexDirection: "column",
       justifyContent: "Flex-start",
       alignItems: "flex-start",
       backgroundColor: "#d1d2d3",
