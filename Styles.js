6820666dd2eb4e826e9d24e7ba254155df1881a8
Kevin R Salger <kevin.salger@student.csulb.edu>
2021-06-05 21:23:38 -0700

    21st

--- Styles.js
+++ Styles.js
@@ -19,10 +19,11 @@ export default StyleSheet.create({
       android: {paddingTop: StatusBar.currentHeight}
     })
   },
-  
-  colbox: {
+  //  Outer box containing "Column" and Child1, Child2, and Child3 boxes
+  colbox: { 
     width: 200,
-    height: 400,
+    //height: 400,
+    flexDirection: "row",
     justifyContent: "stretch",
     alignItems: "center",
     backgroundColor: "#d1d2d3",
@@ -31,7 +32,7 @@ export default StyleSheet.create({
     borderStyle: "solid",
     borderColor: "#000000"  // Black
   }, 
-
+  //  Defines boxes for Child1, Child2, and Child3
   box2: {
       width: 75,
       height: 75,
@@ -43,11 +44,11 @@ export default StyleSheet.create({
       borderStyle: "solid",
       borderColor: "#000000"
   },
-
+  //  Defines outer box for "Row" and Child4, Child5, and Child6 boxes
   rowbox1: {
       //width: 400,
       height: 200,
-      flexDirection: "column",
+      flexDirection: "row",
       justifyContent: "Flex-start",
       alignItems: "flex-start",
       backgroundColor: "#d1d2d3",
@@ -56,7 +57,7 @@ export default StyleSheet.create({
       borderStyle: "solid",
       borderColor: "#000000"
   },
-  
+  //  Internal row containing only the text "Row"
   rowbox2: {
     //width: 400,
     height: 50,
@@ -69,7 +70,7 @@ export default StyleSheet.create({
     borderStyle: "solid",
     borderColor: "#000000"
   },
-
+  //  Internal row containing only the Child4-6 boxes
   rowbox3: {
     width: 200,
     height: 50,
@@ -82,7 +83,7 @@ export default StyleSheet.create({
     borderStyle: "solid",
     borderColor: "#000000"
   },
-
+  //  Defines boxes containing Child4, Child5, and Child6
   box4: {
       width: 75,
       // flexDirection: "column",
